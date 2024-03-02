db2a17bdf8e9ce3e0e3217d6796ef53d11e32492
Xuemin Duan <xueminduan@163.com>
2024-03-02 21:40:24 +0100

    modify fastapi

--- SCOOP-UI/assets/js/scoop.js
+++ SCOOP-UI/assets/js/scoop.js
@@ -1,19 +1,57 @@
-function loadExample(exampleName) {
-    // 使用Ajax请求加载所选例子的内容并填充到文本框中
+function xsdLoadExample(exampleName) {
+
     if (exampleName) {
-        // 假设例子文件存放在名为 "example" 的文件夹下
+
         var filePath = "examples/" + exampleName;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("schemaTextarea").value = this.responseText;
+                document.getElementById("xsdText").value = this.responseText;
             }
         };
         xhttp.open("GET", filePath, true);
         xhttp.send();
     } else {
-        // 如果用户选择了空选项，则清空文本框内容
-        document.getElementById("schemaTextarea").value = "";
+
+        document.getElementById("xsdText").value = "";
+    }
+}
+
+function rmlLoadExample(exampleName) {
+
+    if (exampleName) {
+
+        var filePath = "examples/" + exampleName;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("rmlText").value = this.responseText;
+            }
+        };
+        xhttp.open("GET", filePath, true);
+        xhttp.send();
+    } else {
+
+        document.getElementById("rmlText").value = "";
+    }
+}
+
+function owlLoadExample(exampleName) {
+
+    if (exampleName) {
+
+        var filePath = "examples/" + exampleName;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("owlText").value = this.responseText;
+            }
+        };
+        xhttp.open("GET", filePath, true);
+        xhttp.send();
+    } else {
+
+        document.getElementById("owlText").value = "";
     }
 }
 
@@ -27,30 +65,54 @@ function handleFileUpload(input) {
     reader.readAsText(file);
 }
 
+// function translateIt() {
+
+//     var formData = new FormData();
+//     formData.append('xsdData', document.getElementById('xsdText').value);       
+//     formData.append('rmlData', document.getElementById('rmlText').value);
+//     formData.append('owlData', document.getElementById('owlText').value);
+
+//     // Send POST request to backend
+//     fetch('/translate', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         // Handle the response from backend
+//         console.log('SHACL output:', data);
+//         // document.getElementById('shaclOutput').innerText = JSON.stringify(data, null, 2);
+//         document.getElementById('shaclOutput').innerText = data.shacl_output;
+//         // Update your HTML elements with the SHACL output
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });   
+// } 
 function translateIt() {
 
-    var formData = new FormData();
-    formData.append('xsdFile', document.getElementById('xsdFile').value);
-    formData.append('rmlFile', document.getElementById('rmlFile').value);
-    formData.append('owlFile', document.getElementById('owlFile').value);
-    // var rawDataSchema = document.getElementById('rawDataSchema').value;
-    // var rmlMappingRules = document.getElementById('rmlMappingRules').value;
-    // var ontologyContent = document.getElementById('ontology').value;
+    var requestData = {
+        rmlData: document.getElementById('rmlText').value,
+        owlData: document.getElementById('owlText').value,
+        xsdData: document.getElementById('xsdText').value
+    };
 
-    // Send POST request to backend
-    fetch('/translate', {
+
+    fetch('https://repulsive-loincloth-hare.cyclic.app/translate', { 
         method: 'POST',
-        body: formData
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(requestData) 
     })
     .then(response => response.json())
     .then(data => {
-        // Handle the response from backend
+
         console.log('SHACL output:', data);
-        // document.getElementById('shaclOutput').innerText = JSON.stringify(data, null, 2);
         document.getElementById('shaclOutput').innerText = data.shacl_output;
-        // Update your HTML elements with the SHACL output
+
     })
     .catch(error => {
         console.error('Error:', error);
     });   
-} 
+}
