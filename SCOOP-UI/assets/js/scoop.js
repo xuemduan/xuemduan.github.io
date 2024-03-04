d913f5b402cdc5846e1f4a273775e5bf669f5be7
Xuemin Duan <xueminduan@163.com>
2024-03-04 16:42:45 +0100

    update

--- SCOOP-UI/assets/js/scoop.js
+++ SCOOP-UI/assets/js/scoop.js
@@ -65,40 +65,18 @@ function handleFileUpload(input) {
     reader.readAsText(file);
 }
 
-// function translateIt() {
-
-//     var formData = new FormData();
-//     formData.append('xsdData', document.getElementById('xsdText').value);       
-//     formData.append('rmlData', document.getElementById('rmlText').value);
-//     formData.append('owlData', document.getElementById('owlText').value);
-
-//     // Send POST request to backend
-//     fetch('/translate', {
-//         method: 'POST',
-//         body: formData
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         // Handle the response from backend
-//         console.log('SHACL output:', data);
-//         // document.getElementById('shaclOutput').innerText = JSON.stringify(data, null, 2);
-//         document.getElementById('shaclOutput').innerText = data.shacl_output;
-//         // Update your HTML elements with the SHACL output
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });   
-// } 
+const loadingSpinner = document.getElementById('loadingSpinner');
 function translateIt() {
-
+    loadingSpinner.style.display = 'block';
+    var selectedOption = document.querySelector('input[name="mode"]:checked').value;
     var requestData = {
         rmlData: document.getElementById('rmlText').value,
         owlData: document.getElementById('owlText').value,
-        xsdData: document.getElementById('xsdText').value
+        xsdData: document.getElementById('xsdText').value,
+        mode: selectedOption
     };
-
-
-    fetch('https://good-gray-caterpillar-robe.cyclic.app/translate', { 
+    console.log('Request data:', requestData);
+    fetch('http://193.190.127.194:8000/translate', { 
         method: 'POST',
         headers: {
             'Content-Type': 'application/json' 
@@ -107,12 +85,75 @@ function translateIt() {
     })
     .then(response => response.json())
     .then(data => {
-
-        console.log('SHACL output:', data);
-        document.getElementById('shaclOutput').innerText = data.shacl_output;
-
+        console.log('Success:', data.shacl_output);
+        document.getElementById('shaclOutput').value = data.shacl_output;
+        loadingSpinner.style.display = 'none';
     })
     .catch(error => {
         console.error('Error:', error);
+        loadingSpinner.style.display = 'none';
     });   
 }
+
+
+
+const settingsIcon = document.getElementById('settingsIcon');
+const settingsModal = document.getElementById('settingsModal');
+const closeButton = document.getElementsByClassName('close')[0];
+const saveButton = document.getElementById('saveButton');
+
+settingsIcon.onclick = function() {
+  settingsModal.style.display = 'block';
+}
+
+closeButton.onclick = function() {
+  settingsModal.style.display = 'none';
+}
+
+saveButton.onclick = function() {
+//   const checkboxChecked = document.getElementById('checkbox').checked;
+//   const prioritySelected = document.querySelector('input[name="mode"]:checked').value;
+
+//   console.log('Checkbox checked:', checkboxChecked);
+//   console.log('Priority selected:', prioritySelected);
+
+  settingsModal.style.display = 'none';
+}
+
+
+window.onclick = function(event) {
+  if (event.target == settingsModal) {
+    settingsModal.style.display = 'none';
+  }
+}
+
+// const translateButton = document.getElementById('translateButton');
+// const loadingSpinner = document.getElementById('loadingSpinner');
+
+// translateButton.addEventListener('click', () => {
+
+//   loadingSpinner.style.display = 'block';
+
+
+//   fetch('/translate', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json' 
+//     },
+//     body: JSON.stringify(requestData) 
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+
+//     loadingSpinner.style.display = 'none';
+
+
+//     console.log('Success:', data.shacl_output);
+//     document.getElementById('shaclOutput').value = data.shacl_output;
+//   })
+//   .catch(error => {
+
+//     loadingSpinner.style.display = 'none';
+//     console.error('Error:', error);
+//   });
+// });
